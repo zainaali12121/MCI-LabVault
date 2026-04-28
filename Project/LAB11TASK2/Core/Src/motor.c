0a71ec99c554a8562de2b66dc9fb5feb1953d5a1
aimanasimzubedi <az10271@st.habib.edu.pk>
2026-04-28 23:04:54 +0500

    final changes 28/04/26
    Refactor Motor_Set function to improve clarity and add comments for better understanding of motor control logic.

--- Project/LAB11TASK2/Core/Src/motor.c
+++ Project/LAB11TASK2/Core/Src/motor.c
@@ -1,12 +1,25 @@
 #include "main.h"
 #include <math.h>
-
+// Timer used for PWM generation (motor speed control)
 extern TIM_HandleTypeDef htim3;
 
+/**
+ * @brief  Set motor direction and speed based on control signal
+ * @param  control PID output (can be positive or negative)
+ * 
+ * This function:
+ * 1. Determines motor direction based on sign of control
+ * 2. Converts control magnitude to PWM duty cycle
+ * 3. Applies saturation and minimum threshold
+ * 4. Smooths PWM using a simple low-pass filter
+ * 5. Outputs PWM to motor driver via TIM3
+ */
 void Motor_Set(float control)
-{
+{   // ---------------- Direction Control ----------------
+    // Positive: forward, Negative: reverse
     if(control > 0)
     {
+        // Motor direction: forward
         HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, 1);
         HAL_GPIO_WritePin(GPIOD, GPIO_PIN_8, 0);
 
@@ -15,6 +28,7 @@ void Motor_Set(float control)
     }
     else
     {
+        // Motor direction: reverse
         HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, 0);
         HAL_GPIO_WritePin(GPIOD, GPIO_PIN_8, 1);
 
@@ -22,17 +36,27 @@ void Motor_Set(float control)
         HAL_GPIO_WritePin(GPIOD, GPIO_PIN_7, 1);
     }
 
+    // Magnitude Conversion
+    // Use absolute value since direction is already handled
     int pwm = (int)fabs(control);
 
+    //  Saturation Limit
+    // Prevent excessive speed / protect hardware
     if(pwm > 600) pwm = 600;
 
+      //Minimum Threshold
+    // Ensure motor overcomes static friction (dead zone)
     if(pwm > 0 && pwm < 120)
         pwm = 120;
 
+    //PWM Smoothing(Low-pass filter)
+    // Reduces sudden jerks in motor response
     static float pwm_filtered = 0;
     pwm_filtered = 0.8f * pwm_filtered + 0.2f * pwm;
     pwm = pwm_filtered;
 
-    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, pwm*2);
+     //PWM Output
+    // Channel 1 and 2 control two motor inputs
+    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, pwm*2);  //scaled by 2 manually due to mismatch of the two channels in the motor driver
     __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, pwm);
 }
